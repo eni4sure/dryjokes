b7ffc3b11aee66fbef193f85826f505b3513bd8e
Eniola Osabiya <eosabiya@gmail.com>
2020-04-30 05:06:45 +0100

    Updated API usage

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,16 @@ async function get_joke() {
 	const jokesApi = [ 'https://icanhazdadjoke.com/', 'https://sv443.net/jokeapi/v2/joke/Miscellaneous,Dark?blacklistFlags=nsfw,religious,racist,sexist&type=single', 'https://api.yomomma.info/', 'http://jokes.guyliangilsing.me/retrieveJokes.php?type=yomama', 'http://jokes.guyliangilsing.me/retrieveJokes.php?type=dadjoke' ];
 
 	var random_joke_api = jokesApi[ Math.floor( Math.random() * jokesApi.length ) ];
-	console.log(random_joke_api);
 	
+	if ( [ 'https://api.yomomma.info/', 'http://jokes.guyliangilsing.me/retrieveJokes.php?type=yomama', 'http://jokes.guyliangilsing.me/retrieveJokes.php?type=dadjoke' ].indexOf( random_joke_api ) > -1 ) {
+		
+		var jokeRes = await fetch( random_joke_api );
+	} else {
 
-	const jokeRes = await fetch( random_joke_api, { headers: { 'Accept': 'application/json' } });
-	
-	const jokedata = await jokeRes.json();
+		var jokeRes = await fetch( random_joke_api, { headers: { 'Accept': 'application/json' } });
+	}
+
+	var jokedata = await jokeRes.json();
 	// console.log(joke);
 
 	$("#joke").html( jokedata.joke );
