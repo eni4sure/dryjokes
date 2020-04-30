1596c6c3b15d570283dfbabbdd55797b4c58e69f
Eniola Osabiya <eosabiya@gmail.com>
2020-04-30 06:14:47 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ async function get_joke() {
 
 	var random_joke_api = jokesApi[ Math.floor( Math.random() * jokesApi.length ) ];
 
-	$.getJSON( 'https://cors.io/?' + random_joke_api , function(joke_json) {
+	$.getJSON( 'http://api.allorigins.win/get?url=' + random_joke_api , function(joke_json) {
 		$("#joke").html( joke_json.joke );
 	});
 }
